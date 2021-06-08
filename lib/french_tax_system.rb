2c8c042f400aec7a9179421b2705cd15c0f68af8
Mathieu EUSTACHY <mathieu.eustachy@gmail.com>
2021-06-08 09:31:05 +0200

    Refactored our gem: module and specs

--- lib/french_tax_system.rb
+++ lib/french_tax_system.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
+require "date"
 require_relative "french_tax_system/version"
 
 module FrenchTaxSystem
+  extend self
   class Error < StandardError; end
 
   # Constants
@@ -30,112 +32,96 @@ module FrenchTaxSystem
                                                      house_property_management_amount_per_year house_insurance_gli_amount_per_year house_insurance_pno_amount_per_year house_property_tax_amount_per_year credit_loan_cumulative_interests_paid_for_year_two credit_loan_insurance_amount_per_year].freeze
 
   # Methods
-  ## Main method
-  def testit
-    "test me"
-  end
 
-  def calc_income_tax_amount_per_year_with_property_income_of(net_taxable_property_income_amount)
-    global_net_taxable_income_amount = calc_global_net_taxable_amount_with_property_income_of(net_taxable_property_income_amount)
+  # Calculate the income tax to pay with or without the generated income from the property investment
+  #
+  # @params [Hash] simulation a simulation created by Mini-Keyz app
+  # @options simulation [Integer] :house_price_bought_amount how much was the house bought (euros)
+  # @options simulation [Integer] :house_first_works_amount how much were the first works realized (euros)
+  # @options simulation [Integer] :house_total_charges_amount_per_year how much are the total charges (euros/year)
+  # @options simulation [Integer] :house_property_tax_amount_per_year how much is the property tax (euros/year)
+  # @options simulation [Integer] :house_rent_amount_per_month how much is the rent paid by the tenant (euros/month)
+  # @options simulation [Integer] :house_property_management_amount_per_year how much is property management cost (euros/year)
+  # @options simulation [Integer] :credit_loan_amount how much is credit loan amount (euros)
+  # @options simulation [Integer] :credit_loan_duration how long is the credit (years)
+  # @options simulation [String] :fiscal_status what fiscal status has been chosen
+  # @options simulation [String] :fiscal_regimen what fiscal regimen has been chosen
+  # @options simulation [Integer] :fiscal_revenues_p1 salary from person 1 of the fiscal household (euros)
+  # @options simulation [Integer] :fiscal_revenues_p2 salary from person 2 of the fiscal household (euros)
+  # @options simulation [String] :fiscal_marital_status fiscal relation between the 'parents' of the household
+  # @options simulation [Integer] :fiscal_nb_dependent_children number of dependent children of fiscal household
+  # @options simulation [Integer] :fiscal_nb_alternate_custody_children number of alternate custody children of fiscal household
+  # params [String] indicates if the calculation is made with or without the property income
+  # @return [Float] the final income tax to pay (euros)
+  def calc_income_tax_amount_per_year(simulation, calculation_method)
+    case calculation_method
+    when "with_property_income"
+      net_taxable_property_income_amount = calc_net_taxable_property_income_amount(simulation)
+      global_net_taxable_income_amount = calc_global_net_taxable_amount_with_property_income_of(simulation,
+                                                                                                net_taxable_property_income_amount)
+    when "without_property_income"
+      global_net_taxable_income_amount = calc_global_net_taxable_amount_with_property_income_of(simulation,
+                                                                                                0)
+    else
+      raise ArgumentError, "Not a valid argument, it should be 'with_property_income' or 'without_property_income'"
+    end
 
-    fiscal_nb_parts = calc_fiscal_nb_parts
-
-    family_quotient_amount = calc_family_quotient_amount(global_net_taxable_income_amount, fiscal_nb_parts)
-    current_year = Date.today.year
-
-    aggregated_tax_amount = calc_aggregated_tax_amount(family_quotient_amount, current_year)
-
-    (aggregated_tax_amount * fiscal_nb_parts).floor
-  end
-
-  def calc_not_capped_income_tax_amount_per_year_with_property_income_of(net_taxable_property_income_amount)
-    global_net_taxable_income_amount = calc_global_net_taxable_amount_with_property_income_of(net_taxable_property_income_amount)
-
-    fiscal_nb_parts = calc_fiscal_nb_parts
-
-    family_quotient_amount = calc_family_quotient_amount(global_net_taxable_income_amount, fiscal_nb_parts)
-    current_year = Date.today.year
-
-    aggregated_tax_amount = calc_aggregated_tax_amount(family_quotient_amount, current_year)
-
-    (aggregated_tax_amount * fiscal_nb_parts).floor
-  end
-
-  def calc_capped_income_tax_amount_per_year_with_property_income_of(net_taxable_property_income_amount)
-    global_net_taxable_income_amount = calc_global_net_taxable_amount_with_property_income_of(net_taxable_property_income_amount)
-
-    fiscal_nb_parts = case fiscal_marital_status
-                      when "Célibataire" then 1
-                      when "Marié / Pacsé" then 2
-                      end
+    fiscal_nb_parts = calc_fiscal_nb_parts(simulation)
 
     family_quotient_amount = calc_family_quotient_amount(global_net_taxable_income_amount, fiscal_nb_parts)
     current_year = Date.today.year
 
     aggregated_tax_amount = calc_aggregated_tax_amount(family_quotient_amount, current_year)
 
-    (aggregated_tax_amount * fiscal_nb_parts).floor
+    (aggregated_tax_amount * fiscal_nb_parts).round
   end
 
-  def calc_net_taxable_property_income_amount
+  def calc_net_taxable_property_income_amount(simulation)
     case fiscal_status
     when "Vide"
-      NueFormulas.calc_net_taxable_property_income_amount(self)
+      NueFormulas.calc_net_taxable_property_income_amount(simulation)
     when "LMNP"
-      LmnpFormulas.calc_net_taxable_property_income_amount(self)
+      LmnpFormulas.calc_net_taxable_property_income_amount(simulation)
     end
   end
 
-  def calc_income_taxes_scale_with_property_income_of(net_taxable_property_income_amount)
-    global_net_taxable_income_amount = calc_global_net_taxable_amount_with_property_income_of(net_taxable_property_income_amount)
-
-    fiscal_nb_parts = calc_fiscal_nb_parts
-
-    family_quotient_amount = calc_family_quotient_amount(global_net_taxable_income_amount, fiscal_nb_parts)
-    current_year = Date.today.year
-
-    income_taxes_scale = INCOME_TAXES_SCALE["year#{current_year}".to_sym]
-
-    income_taxes_scale.find { |scale| family_quotient_amount <= scale[:family_quotient_amount][:end_scale] }
+  def calc_global_net_taxable_amount(simulation, net_taxable_property_income_amount)
+    ((simulation[:fiscal_revenues_p1] + simulation[:fiscal_revenues_p2]) * (1 - REVENUES_STANDARD_ALLOWANCE)) + net_taxable_property_income_amount
   end
 
-  def calc_global_net_taxable_amount_with_property_income_of(net_taxable_property_income_amount)
-    ((fiscal_revenues_p1 + fiscal_revenues_p2) * (1 - REVENUES_STANDARD_ALLOWANCE)) + net_taxable_property_income_amount
-  end
-
-  def calc_fiscal_nb_parts
-    case fiscal_marital_status
+  def calc_fiscal_nb_parts(simulation)
+    case simulation[:fiscal_marital_status]
     when "Marié / Pacsé"
-      FISCAL_NB_PARTS_FOR_MARRIED_COUPLE + calc_fiscal_nb_parts_incurred_from_children
+      FISCAL_NB_PARTS_FOR_MARRIED_COUPLE + calc_fiscal_nb_parts_incurred_from_children(simulation)
     when "Célibataire"
-      FISCAL_NB_PARTS_FOR_SINGLE_PERSON + calc_fiscal_nb_parts_incurred_from_children
+      FISCAL_NB_PARTS_FOR_SINGLE_PERSON + calc_fiscal_nb_parts_incurred_from_children(simulation)
     end
   end
 
-  def calc_fiscal_nb_parts_incurred_from_children
+  def calc_fiscal_nb_parts_incurred_from_children(simulation)
     # Detail of calculation method in GitHub wiki: https://github.com/Mth0158/mini-keyz/wiki/French-tax-system
-    total_nb_children = fiscal_nb_dependent_children + fiscal_nb_alternate_custody_children
+    total_nb_children = simulation[:fiscal_nb_dependent_children] + simulation[:fiscal_nb_alternate_custody_children]
 
     if total_nb_children <= 2
-      0.5 * fiscal_nb_dependent_children + 0.25 * fiscal_nb_alternate_custody_children
+      0.5 * simulation[:fiscal_nb_dependent_children] + 0.25 * simulation[:fiscal_nb_alternate_custody_children]
 
     elsif total_nb_children >= 3
 
-      if fiscal_nb_dependent_children == 0
-        0.5 + (fiscal_nb_alternate_custody_children - 2) * 0.5
+      if simulation[:fiscal_nb_dependent_children] == 0
+        0.5 + (simulation[:fiscal_nb_alternate_custody_children] - 2) * 0.5
 
-      elsif fiscal_nb_dependent_children == 1
-        if fiscal_nb_alternate_custody_children == 2
+      elsif simulation[:fiscal_nb_dependent_children] == 1
+        if simulation[:fiscal_nb_alternate_custody_children] == 2
           1.25
-        elsif fiscal_nb_alternate_custody_children >= 3
-          1.25 + (fiscal_nb_alternate_custody_children - 2) * 0.5
+        elsif simulation[:fiscal_nb_alternate_custody_children] >= 3
+          1.25 + (simulation[:fiscal_nb_alternate_custody_children] - 2) * 0.5
         end
 
-      elsif fiscal_nb_dependent_children == 2
-        1 + fiscal_nb_alternate_custody_children * 0.5
+      elsif simulation[:fiscal_nb_dependent_children] == 2
+        1 + simulation[:fiscal_nb_alternate_custody_children] * 0.5
 
-      elsif fiscal_nb_dependent_children >= 3
-        2 + fiscal_nb_alternate_custody_children * 0.5
+      elsif simulation[:fiscal_nb_dependent_children] >= 3
+        2 + simulation[:fiscal_nb_alternate_custody_children] * 0.5
       end
     end
   end
@@ -160,5 +146,27 @@ module FrenchTaxSystem
         (scale[:family_quotient_amount][:end_scale] - scale[:family_quotient_amount][:start_scale]) * scale[:tax]
       end
     end.sum
+  end
+
+  def calc_income_taxes_scale(simulation, calculation_method)
+    case calculation_method
+    when "with_property_income"
+      net_taxable_property_income_amount = calc_net_taxable_property_income_amount(simulation)
+      global_net_taxable_income_amount = calc_global_net_taxable_amount_with_property_income_of(simulation,
+                                                                                                net_taxable_property_income_amount)
+    when "without_property_income"
+      global_net_taxable_income_amount = calc_global_net_taxable_amount_with_property_income_of(simulation, 0)
+    else
+      raise ArgumentError, "Not a valid argument, it should be 'with_property_income' or 'without_property_income'"
+    end
+
+    fiscal_nb_parts = calc_fiscal_nb_parts(simulation)
+
+    family_quotient_amount = calc_family_quotient_amount(global_net_taxable_income_amount, fiscal_nb_parts)
+    current_year = Date.today.year
+
+    income_taxes_scale = INCOME_TAXES_SCALE["year#{current_year}".to_sym]
+
+    income_taxes_scale.find { |scale| family_quotient_amount <= scale[:family_quotient_amount][:end_scale] }
   end
 end
--- spec/french_tax_system_spec.rb
+++ spec/french_tax_system_spec.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
 RSpec.describe FrenchTaxSystem do
-  it 'has a version number' do
+  it "has a version number" do
     expect(FrenchTaxSystem::VERSION).not_to be nil
   end
 
   let(:simulation_lyon) do
     {
-      house_city: 'Lyon 01',
+      house_city: "Lyon 01",
       house_price_bought_amount: 500_000,
       house_first_works_amount: 25_000,
       house_total_charges_amount_per_year: 2750,
@@ -16,10 +16,11 @@ RSpec.describe FrenchTaxSystem do
       house_property_management_cost_percentage: 0,
       credit_loan_amount: 550_000,
       credit_loan_duration: 20,
-      fiscal_status: 'Vide',
-      fiscal_regimen: 'Réel',
+      fiscal_status: "Vide",
+      fiscal_regimen: "Réel",
       fiscal_revenues_p1: 75_000,
       fiscal_revenues_p2: 25_000,
+      fiscal_marital_status: "Marié / Pacsé",
       fiscal_nb_dependent_children: 2,
       fiscal_nb_alternate_custody_children: 1
     }
@@ -27,7 +28,7 @@ RSpec.describe FrenchTaxSystem do
 
   let(:simulation_bordeaux) do
     {
-      house_city: 'Bordeaux',
+      house_city: "Bordeaux",
       house_price_bought_amount: 100_000,
       house_first_works_amount: 0,
       house_total_charges_amount_per_year: 500,
@@ -36,10 +37,11 @@ RSpec.describe FrenchTaxSystem do
       house_property_management_cost_percentage: 0.08,
       credit_loan_amount: 100_000,
       credit_loan_duration: 20,
-      fiscal_status: 'LMNP',
-      fiscal_regimen: 'Réel',
+      fiscal_status: "LMNP",
+      fiscal_regimen: "Réel",
       fiscal_revenues_p1: 25_000,
       fiscal_revenues_p2: 35_000,
+      fiscal_marital_status: "Marié / Pacsé",
       fiscal_nb_dependent_children: 3,
       fiscal_nb_alternate_custody_children: 0
     }
@@ -47,7 +49,7 @@ RSpec.describe FrenchTaxSystem do
 
   let(:simulation_nimes) do
     {
-      house_city: 'Nîmes',
+      house_city: "Nîmes",
       house_price_bought_amount: 300_000,
       house_first_works_amount: 5_000,
       house_total_charges_amount_per_year: 1_500,
@@ -56,10 +58,11 @@ RSpec.describe FrenchTaxSystem do
       house_property_management_cost_percentage: 0.08,
       credit_loan_amount: 200_000,
       credit_loan_duration: 20,
-      fiscal_status: 'Meublé',
-      fiscal_regimen: 'Forfait',
+      fiscal_status: "Meublé",
+      fiscal_regimen: "Forfait",
       fiscal_revenues_p1: 80_000,
       fiscal_revenues_p2: 65_000,
+      fiscal_marital_status: "Marié / Pacsé",
       fiscal_nb_dependent_children: 2,
       fiscal_nb_alternate_custody_children: 3
     }
@@ -67,7 +70,7 @@ RSpec.describe FrenchTaxSystem do
 
   let(:simulation_lille) do
     {
-      house_city: 'Lille',
+      house_city: "Lille",
       house_price_bought_amount: 150_000,
       house_first_works_amount: 15_500,
       house_total_charges_amount_per_year: 1_200,
@@ -76,9 +79,10 @@ RSpec.describe FrenchTaxSystem do
       house_property_management_cost_percentage: 0,
       credit_loan_amount: 180_000,
       credit_loan_duration: 20,
-      fiscal_status: 'Vide',
-      fiscal_regimen: 'Réel',
+      fiscal_status: "Vide",
+      fiscal_regimen: "Réel",
       fiscal_revenues_p1: 55_000,
+      fiscal_marital_status: "Célibataire",
       fiscal_nb_dependent_children: 0,
       fiscal_nb_alternate_custody_children: 4
     }
@@ -86,7 +90,7 @@ RSpec.describe FrenchTaxSystem do
 
   let(:simulation_agen) do
     {
-      house_city: 'Agen',
+      house_city: "Agen",
       house_price_bought_amount: 150_000,
       house_first_works_amount: 15_500,
       house_total_charges_amount_per_year: 1_200,
@@ -95,73 +99,42 @@ RSpec.describe FrenchTaxSystem do
       house_property_management_cost_percentage: 0,
       credit_loan_amount: 180_000,
       credit_loan_duration: 20,
-      fiscal_status: 'Vide',
-      fiscal_regimen: 'Réel',
+      fiscal_status: "Vide",
+      fiscal_regimen: "Réel",
       fiscal_revenues_p1: 61_111,
       fiscal_revenues_p2: 50_000,
+      fiscal_marital_status: "Marié / Pacsé",
       fiscal_nb_dependent_children: 3,
       fiscal_nb_alternate_custody_children: 0
     }
   end
 
-  describe '#calc_not_capped_income_tax_amount_per_year_with_property_income_of(net_taxable_property_income_amount)' do
-    context 'when it has no property income' do
-      it 'returns not capped by fiscal parts capping income tax per year with a given property income' do
-        result_agen = FrenchTaxSystem.calc_not_capped_income_tax_amount_per_year_with_property_income_of(simulation_agen)
-        expect(result_agen).to be_within(1).of(6_562.6)
-      end
-    end
-
-    context 'when it has some property income' do
-      it 'returns not capped by fiscal parts capping income tax per year with a given property income' do
-        result_agen = simulation_agen.calc_not_capped_income_tax_amount_per_year_with_property_income_of(20_000)
-        expect(result_agen).to be_within(1).of(12_021.8)
-      end
-    end
-  end
-
-  describe '#calc_capped_income_tax_amount_per_year_with_property_income_of(net_taxable_property_income_amount)' do
-    context 'when it has no property income' do
-      it 'returns capped by fiscal parts capping income tax per year with a given property income' do
-        result_agen = simulation_agen.calc_not_capped_income_tax_amount_per_year_with_property_income_of(0)
-        expect(result_agen).to be_within(1).of(18_011)
-      end
-    end
-
-    context 'when it has some property income' do
-      it 'returns capped by fiscal parts capping income tax per year with a given property income' do
-        result_agen = simulation_agen.calc_not_capped_income_tax_amount_per_year_with_property_income_of(20_000)
-        expect(result_agen).to be_within(1).of(12_021.8)
-      end
-    end
-  end
-
-  describe '#calc_global_net_taxable_amount_with_property_income_of(net_taxable_property_income)' do
-    context 'when it has no property income' do
-      it 'returns the net taxable amount' do
-        result_lyon = simulation_lyon.calc_global_net_taxable_amount_with_property_income_of(0)
-        result_bordeaux = simulation_bordeaux.calc_global_net_taxable_amount_with_property_income_of(0)
+  describe "#calc_global_net_taxable_amount(simulation, net_taxable_property_income)" do
+    context "when it has no property income" do
+      it "returns the net taxable amount" do
+        result_lyon = FrenchTaxSystem.calc_global_net_taxable_amount(simulation_lyon, 0)
+        result_bordeaux = FrenchTaxSystem.calc_global_net_taxable_amount(simulation_bordeaux, 0)
         expect(result_lyon).to be_within(0.01).of(90_000)
         expect(result_bordeaux).to be_within(0.01).of(54_000)
       end
     end
 
-    context 'when it has some property income' do
-      it 'returns the net taxable amount' do
-        result_lyon = simulation_lyon.calc_global_net_taxable_amount_with_property_income_of(20_000)
-        result_bordeaux = simulation_bordeaux.calc_global_net_taxable_amount_with_property_income_of(2_000)
+    context "when it has some property income" do
+      it "returns the net taxable amount" do
+        result_lyon = FrenchTaxSystem.calc_global_net_taxable_amount(simulation_lyon, 20_000)
+        result_bordeaux = FrenchTaxSystem.calc_global_net_taxable_amount(simulation_bordeaux, 2_000)
         expect(result_lyon).to be_within(0.01).of(110_000)
         expect(result_bordeaux).to be_within(0.01).of(56_000)
       end
     end
   end
 
-  describe '#calc_fiscal_nb_parts' do
+  describe "#calc_fiscal_nb_parts(simulation)" do
     it "returns household's fiscal parts number" do
-      result_lyon = simulation_lyon.calc_fiscal_nb_parts
-      result_bordeaux = simulation_bordeaux.calc_fiscal_nb_parts
-      result_nimes = simulation_nimes.calc_fiscal_nb_parts
-      result_lille = simulation_lille.calc_fiscal_nb_parts
+      result_lyon = FrenchTaxSystem.calc_fiscal_nb_parts(simulation_lyon)
+      result_bordeaux = FrenchTaxSystem.calc_fiscal_nb_parts(simulation_bordeaux)
+      result_nimes = FrenchTaxSystem.calc_fiscal_nb_parts(simulation_nimes)
+      result_lille = FrenchTaxSystem.calc_fiscal_nb_parts(simulation_lille)
       expect(result_lyon).to eq(3.5)
       expect(result_bordeaux).to eq(4)
       expect(result_nimes).to eq(4.5)
@@ -169,12 +142,12 @@ RSpec.describe FrenchTaxSystem do
     end
   end
 
-  describe '#calc_fiscal_nb_parts_incurred_from_children' do
+  describe "#calc_fiscal_nb_parts_incurred_from_children(simulation)" do
     it "returns household's fiscal parts number incurred from dependant and alternate custody children" do
-      result_lyon = simulation_lyon.calc_fiscal_nb_parts_incurred_from_children
-      result_bordeaux = simulation_bordeaux.calc_fiscal_nb_parts_incurred_from_children
-      result_nimes = simulation_nimes.calc_fiscal_nb_parts_incurred_from_children
-      result_lille = simulation_lille.calc_fiscal_nb_parts_incurred_from_children
+      result_lyon = FrenchTaxSystem.calc_fiscal_nb_parts_incurred_from_children(simulation_lyon)
+      result_bordeaux = FrenchTaxSystem.calc_fiscal_nb_parts_incurred_from_children(simulation_bordeaux)
+      result_nimes = FrenchTaxSystem.calc_fiscal_nb_parts_incurred_from_children(simulation_nimes)
+      result_lille = FrenchTaxSystem.calc_fiscal_nb_parts_incurred_from_children(simulation_lille)
       expect(result_lyon).to eq(1.5)
       expect(result_bordeaux).to eq(2)
       expect(result_nimes).to eq(2.5)
@@ -182,33 +155,33 @@ RSpec.describe FrenchTaxSystem do
     end
   end
 
-  describe '#calc_family_quotient_amount' do
-    context 'when it has no property income' do
-      it 'returns the family quotient amount' do
-        result_lyon = simulation_lyon.calc_family_quotient_amount(90_000, simulation_lyon.fiscal_nb_parts)
-        result_bordeaux = simulation_bordeaux.calc_family_quotient_amount(54_000, simulation_bordeaux.fiscal_nb_parts)
-        expect(result_lyon).to be_within(0.01).of(30_000)
+  describe "#calc_family_quotient_amount(global_net_taxable_income_amount, fiscal_nb_parts)" do
+    context "when it has no property income" do
+      it "returns the family quotient amount" do
+        result_lyon = FrenchTaxSystem.calc_family_quotient_amount(90_000, 3.5)
+        result_bordeaux = FrenchTaxSystem.calc_family_quotient_amount(54_000, 4)
+        expect(result_lyon).to be_within(0.01).of(25_714.28)
         expect(result_bordeaux).to be_within(0.01).of(13_500)
       end
     end
 
-    context 'when it has some property income' do
-      it 'returns the family quotient amount' do
-        result_lyon = simulation_lyon.calc_family_quotient_amount(110_000, simulation_lyon.fiscal_nb_parts)
-        result_bordeaux = simulation_bordeaux.calc_family_quotient_amount(56_000, simulation_bordeaux.fiscal_nb_parts)
-        expect(result_lyon).to be_within(0.01).of(36_666.67)
+    context "when it has some property income" do
+      it "returns the family quotient amount" do
+        result_lyon = FrenchTaxSystem.calc_family_quotient_amount(110_000, 3.5)
+        result_bordeaux = FrenchTaxSystem.calc_family_quotient_amount(56_000, 4)
+        expect(result_lyon).to be_within(0.01).of(31_428.57)
         expect(result_bordeaux).to be_within(0.01).of(14_000)
       end
     end
   end
 
-  describe '#calc_aggregated_tax_amount' do
-    it 'returns the aggregated tax amount' do
+  describe "#calc_aggregated_tax_amount(family_quotient_amount, current_year)" do
+    it "returns the aggregated tax amount" do
       current_year = Date.today.year
-      result_lyon = simulation_lyon.calc_aggregated_tax_amount(30_000, current_year)
-      result_bordeaux = simulation_bordeaux.calc_aggregated_tax_amount(13_500, current_year)
-      result_lyon_with_property_income = simulation_lyon.calc_aggregated_tax_amount(36_666.67, current_year)
-      result_bordeaux_with_property_income = simulation_bordeaux.calc_aggregated_tax_amount(14_000, current_year)
+      result_lyon = FrenchTaxSystem.calc_aggregated_tax_amount(30_000, current_year)
+      result_bordeaux = FrenchTaxSystem.calc_aggregated_tax_amount(13_500, current_year)
+      result_lyon_with_property_income = FrenchTaxSystem.calc_aggregated_tax_amount(36_666.67, current_year)
+      result_bordeaux_with_property_income = FrenchTaxSystem.calc_aggregated_tax_amount(14_000, current_year)
       expect(result_lyon).to be_within(0.01).of(3005.45)
       expect(result_bordeaux).to be_within(0.01).of(375.65)
       expect(result_lyon_with_property_income).to be_within(0.01).of(5005.45)
@@ -216,20 +189,20 @@ RSpec.describe FrenchTaxSystem do
     end
   end
 
-  describe '#calc_income_taxes_scale_with_property_income_of' do
-    context 'when it has no property income' do
-      it 'returns the family quotient amount' do
-        result_lyon = simulation_lyon.calc_income_taxes_scale_with_property_income_of(0)
-        result_bordeaux = simulation_bordeaux.calc_income_taxes_scale_with_property_income_of(0)
+  describe "#calc_income_taxes_scale(simulation, calculation_method)" do
+    context "when it has no property income" do
+      it "returns the family quotient amount" do
+        result_lyon = FrenchTaxSystem.calc_income_taxes_scale(simulation_lyon, "without_property_income")
+        result_bordeaux = FrenchTaxSystem.calc_income_taxes_scale(simulation_bordeaux, "without_property_income")
         expect(result_lyon).to eq({ family_quotient_amount: { start_scale: 25_711, end_scale: 73_516 }, tax: 0.3 })
         expect(result_bordeaux).to eq({ family_quotient_amount: { start_scale: 10_085, end_scale: 25_710 }, tax: 0.11 })
       end
     end
 
-    context 'when it has some property income' do
-      it 'returns the family quotient amount' do
-        result_lyon = simulation_lyon.calc_income_taxes_scale_with_property_income_of(20_000)
-        result_bordeaux = simulation_bordeaux.calc_income_taxes_scale_with_property_income_of(2_000)
+    context "when it has some property income" do
+      it "returns the family quotient amount" do
+        result_lyon = FrenchTaxSystem.calc_income_taxes_scale(simulation_lyon, "with_property_income")
+        result_bordeaux = FrenchTaxSystem.calc_income_taxes_scale(simulation_bordeaux, "with_property_income")
         expect(result_lyon).to eq({ family_quotient_amount: { start_scale: 25_711, end_scale: 73_516 }, tax: 0.3 })
         expect(result_bordeaux).to eq({ family_quotient_amount: { start_scale: 10_085, end_scale: 25_710 }, tax: 0.11 })
       end
